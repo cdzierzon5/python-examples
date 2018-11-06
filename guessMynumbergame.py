f89092a1fade43f453c64f7039437fcd4d38f066
Cody Dzierzon <cody.dzierzon@tooeleschools.org>
2018-11-06 14:14:26 -0700

    index info and updates on guess my number

--- guessMynumbergame.py
+++ guessMynumbergame.py
@@ -2,93 +2,119 @@
 #cody and nick
 #11/2/2018
 
+import random
 
-import Random
-global attempts
-global maxTries
-global intRange
-global gNumber
 attempts = 0
 maxTries = 5
-intRange = “1-100”
+intRange = "1-100"
 gNumber = random.randrange(0, 100)
 
-
 def menu():
+    #main menu
     while True:
         print("This is a guess my number game.")
-        start = input(“ Would you like to play? [play, credits, or quit]”)
-        if start.lower() == “play”:
-            custom = input(“Do you want to create your own parameters for the game? y/n”)
-            if custom.lower() == “y”:
+        start = input("Would you like to play? [play, credits, or quit] ")
+        if start.lower() == "play":
+            custom = input("Do you want to create your own parameters for the game? y/n ")
+            if custom.lower() == "y":
                 return options()
-            if custom.lower == “n’:
+            if custom.lower() == "n":
                 return play()
-        if start.lower() == “credits”:
+        if start.lower() == "credits":
                 return credits()
-        if start.lower() == “quit”:
+        if start.lower() == "quit":
             input("Press enter to quit")
             exit()
         else:
             print("Invalid Option")
 
-    
-
-
+#play allows for the player to guess a number
 def play():
-    guess_1 = input(“What is your guess?”)
-    if guess_1.digit():
-        if guess_1 == gNumber:
-            print(“You WON!”)
+    global attempts
+    global maxTries
+    global gNumber
+    while True:
+        if attempts >= int(maxTries):
+            print("You ran out of attempts!")
+            maxTries = 5
+            gNumber = random.randrange(0, 100)
             return menu()
-        if guess_1 <= gNumber:
-            print("incorrect")
-            print("guess higher")
-            attempts += 1
-        if guess_1 >= gNumber:
-            print("incorrect")
-            print("guess lower")
-            attemts += 1
-    
+        guess_1 = int(input("What is your guess? "))
+        if guess_1:
+            if guess_1 == gNumber:
+                print("You WON!")
+                attempts = 0
+                return menu()
+            else:
+                if guess_1 < gNumber:
+                    print("incorrect")
+                    print("guess higher")
+                    attempts += 1
+                    
+                elif guess_1 > gNumber:
+                    print("incorrect")
+                    print("guess lower")
+                    attempts += 1
+                    
+        
     else:
         print("that is not a valid answer")
         return play()
-    
 
+#options allows for changing in the attepts and range
 def options():
-    print("Max Tries is",maxTries+".")
-    print("Range is",intRange+".")
+    global maxTries
+    global intRange
+    global gNumber
+    global attempts
+    print("Max Tries is",str(maxTries)+".")
+    print("Range is",str(intRange)+".")
     yn = input("Do you want to change max tries? [y/n]: ")
     if yn.lower() == "y":
-        maxTries = input("Enter new Max tries: ")
-    elif yn.lower() == "n":
+        newmaxTries = input("Enter new Max tries: ")
+        maxTries = newmaxTries
+        print("maxtries: ",maxTries)
+        #attempts = newmaxTries
 
-        yn = input("Do you want to change range? [y/n]: ")
-        if yn.lower() == "y":
-            tempRange = input("’1-100’ or ‘1-200’ or ‘1-300’")
-            If tempRange == “1-100”:
-                intRange = "1-100”
-                print("range changed to 1-100")
-            
-            elif tempRange == “1-200”:
-                intRange = "1-200”
-                print("range changed to 1-200")
-            
-            elif tempRange == “1-300”:
-                intRange = "1-300”
-                print("range changed to 1-300")
-            
-            else:
-                print(“I don’t know what you mean”)
+        
+    if yn.lower() == "n":
+        print("you said no")
+    yn2 = input("Do you want to change range? [y/n]: ")
+    if yn2.lower() == "y":
+        tempRange = input("’1-100’ or ‘1-200’ or ‘1-300’")
+        if tempRange == "1-100":
+            intRange = "1-100"
+            gNumber = random.randrange(0, 100)
+            print("range changed to 1-100")
+            return play()
+        
+        elif tempRange == "1-200":
+            intRange = "1-200"
+            gNumber = random.randrange(0, 200)
+            print("range changed to 1-200")
+            return play()
+        
+        elif tempRange == "1-300":
+            intRange = "1-300"
+            gNumber = random.randrange(0, 300)
+            print("range changed to 1-300")
+            return play()
+        if tempRange.lower() == "n":
+            print("you didn't change the range")
+            return play
         else:
-            print("i dont know what that means")
+            print("I don’t know what you mean")
+    else:
+        print("i dont know what that means")
 
-
+#credits showing who made the game and when
 def credits():
     print("This game was created on 11/2/2018 by Nick and Cody")
     cEnd = input("would you like to return to the menu?[y/n]")
     if cEnd.lower() == "y":
         return menu()
     else:
-        print("to bad")
+        print("too bad")
         return menu()
+
+menu()
---
+++ indexpractice.py
@@ -0,0 +1,64 @@
+import random
+
+name = input("enter a name ")
+print(name[0])
+length = len(name)
+print(length)
+index_pos = random.randrange(0,length)
+if index_pos <= length:
+    char = name[index_pos]
+else:
+    print("that's out of the range")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##print(name[0])
+##print(name[1])
+##print(name[2])
+##print(name[3])
+##print(name[4])
+##print(name[5])
+##print(name[6])
+##print(name[7])
+##print(name[8])
+##print(name[9])
+##print(name[10])
+##print(name[11])
+##print(name[12])
+##
+##print()
+##
+##print(name[-1])
+##print(name[-2])
+##print(name[-3])
+##print(name[-4])
+##print(name[-5])
+##print(name[-6])
+##print(name[-7])
+##print(name[-8])
+##print(name[-9])
+##print(name[-10])
+##print(name[-11])
+##print(name[-12])
+##print(name[-13])
+
